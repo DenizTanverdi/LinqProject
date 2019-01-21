efa4dda8987417f8df975d5d42f02531772770bd
semvs <semvs@ayvansaray.edu.tr>
2019-01-21 09:19:28 +0300

    Linq

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NortwindDataContext db = new NortwindDataContext();
+            #region Insert
+            //Employee e = new Employee();
+            //e.FirstName = "deniz";
+            //e.LastName = "tanrıverdi";
+            //e.City = "istanbul";
+            //e.Country = "turkey";
+            //e.HomePhone = "798643133";
+            //e.Address="xyz";
+            //db.Employees.InsertOnSubmit(e);
+            //db.SubmitChanges();
+            #endregion
+
+            #region Update
+            
+
+
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
