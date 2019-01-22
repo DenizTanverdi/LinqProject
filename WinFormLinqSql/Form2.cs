96f642d53511271f35becac33d54facefe3b323d
DenizTanrıverdi <semvs@ayvansaray.edu.tr>
2019-01-22 10:05:18 +0300

    Form2

--- WinFormLinqSql/Form2.cs
+++ WinFormLinqSql/Form2.cs
@@ -12,9 +12,36 @@ namespace WinFormLinqSql
 {
     public partial class Form2 : Form
     {
+        NORTHWND2DataContext db = new NORTHWND2DataContext();
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            var categori = (from ctg in db.Categories select ctg).ToList();
+            comboBox1.DataSource = categori;
+            comboBox1.ValueMember = "CategoryID";
+            comboBox1.DisplayMember = "CategoryName";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int Id = Convert.ToInt32(comboBox1.SelectedValue.ToString());
+                var product = (from prdct in db.Products where prdct.CategoryID == Id select prdct).ToList();
+                dataGridView1.DataSource = product;
+                
+            }
+            catch (Exception)
+            {
+
+               
+            }
+            
+         
         }
     }
 }
